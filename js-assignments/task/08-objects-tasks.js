63184d87eb865d85abd979a7dfe1f3baa5108fe8
Aliaksandr Taranishyn <Aliaksandr_Taranishyn@epam.com>
2019-11-26 18:20:00 +0300

    some fix in object task

--- js-assignments/task/08-objects-tasks.js
+++ js-assignments/task/08-objects-tasks.js
@@ -25,9 +25,9 @@
 function Rectangle(width, height) {
   this.width =  width;
   this.height = height;
-  this.getArea = function() {
-    return this.width * this.height;
-  }
+}
+Rectangle.prototype.getArea = function() {
+  return this.width * this.height;
 }
 
 
